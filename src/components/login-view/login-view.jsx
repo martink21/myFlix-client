57d9ae8145bf222c0315800f8f8b33411f2e9581
Martin Karastoyanov <martin.karastoyanov@gmail.com>
2021-07-13 01:05:01 +0200

    Added authentication

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,44 +2,55 @@ import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
+import axios from 'axios';
+
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 
 export function LoginView(props) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(username, password);
-    // Send a request to the server for authentication, then call props.onLoggedIn(username)
-    props.onLoggedIn(username);
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        /* Send a request to the server for authentication */
+        axios.post('https://myflix-21.herokuapp.com/login', {
+            Username: username,
+            Password: password
+        })
+            .then(response => {
+                const data = response.data;
+                props.onLoggedIn(data);
+            })
+            .catch(e => {
+                console.log('no such user')
+            });
+    };
 
 
-  return (
-    <Row className="main-view justify-content-md-center">
-    <Col md={8}>
-    <Form>
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-      </Form.Group>
+    return (
+        <Row className="main-view justify-content-md-center">
+            <Col md={8}>
+                <Form>
+                    <Form.Group controlId="formUsername">
+                        <Form.Label>Username:</Form.Label>
+                        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+                    </Form.Group>
 
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick={handleSubmit}>
-        Submit
-      </Button>
+                    <Form.Group controlId="formPassword">
+                        <Form.Label>Password:</Form.Label>
+                        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" onClick={handleSubmit}>
+                        Submit
+                    </Button>
 
-      <Button variant="outline-secondary" className="float-right" type="button" 
-        onClick={props.toggleRegister}>Register</Button>
-      
-    </Form>
-    </Col>
-    </Row>
-  );
+                    <Button variant="outline-secondary" className="float-right" type="button"
+                        onClick={props.toggleRegister}>Register</Button>
+
+                </Form>
+            </Col>
+        </Row>
+    );
 }
