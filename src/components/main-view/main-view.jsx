2feaa8b54e5aa6e8ecfcd33891705e3a3a11ba9d
Martin Karastoyanov <martin.karastoyanov@gmail.com>
2021-07-15 01:16:25 +0200

    Imported the views

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import axios from 'axios';
 
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-import { Link } from "react-router-dom";
 
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { RegistrationView } from '../registration-view/registration-view';
+import { GenreView } from '../genre-view/genre-view';
+import { DirectorView } from '../director-view/director-view';
+import { ProfileView } from '../profile-view/profile-view';
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -49,7 +51,8 @@ export class MainView extends React.Component {
     }
   }
 
-  /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
+  /* When a user successfully logs in, this function updates the `user` property 
+     in state to that *particular user*/
 
   onLoggedIn(authData) {
     console.log(authData);
@@ -94,7 +97,8 @@ export class MainView extends React.Component {
               <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
             </Col>
             return <Col md={8}>
-              <MovieView movie={movies.find(m => m._id === match.params.movieId)} onBackClick={() => history.goBack()} />
+              <MovieView movie={movies.find(m => m._id === match.params.movieId)} 
+               onBackClick={() => history.goBack()} />
             </Col>
           }} />
 
@@ -104,7 +108,8 @@ export class MainView extends React.Component {
               <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
             </Col>
             return <Col md={8}>
-              <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director} onBackClick={() => history.goBack()} />
+              <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director} 
+               onBackClick={() => history.goBack()} />
             </Col>
           }
           } />
@@ -127,7 +132,8 @@ export class MainView extends React.Component {
               <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
             </Col>
             return <Col md={8}>
-              <GenreView genre={movies.find(m => m.Genre.Name === match.params.name).Genre} onBackClick={() => history.goBack()} />
+              <GenreView genre={movies.find(m => m.Genre.Name === match.params.name).Genre} 
+              onBackClick={() => history.goBack()} />
             </Col>
           }} />
 
