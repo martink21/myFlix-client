75b542c7e8e11f55c8caba0136867daf06fc635e
Martin Karastoyanov <martin.karastoyanov@gmail.com>
2021-07-22 02:51:23 +0200

    Updated profile-view

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,157 +1,295 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import axios from 'axios';
+import PropTypes from 'prop-types';
+import { Card, FormControl } from 'react-bootstrap';
+import axios from "axios";
+import Container from "react-bootstrap/Container";
+import { Button, Form, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
+import { Jumbotron } from 'react-bootstrap';
+import { connect } from 'react-redux';
 import './profile-view.scss';
 
 export class ProfileView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Username: "",
+      Password: "",
+      Email: "",
+      Birthdate: "",
+      FavoriteMovies: [],
+      UsernameError: "",
+      EmailError: "",
+      PasswordError: "",
+      BirthdateError: "",
+    };
+  }
+  componentDidMount() {
+    let accessToken = localStorage.getItem("token");
+    this.getUsers(accessToken);
+  }
 
-  render() {
-    let { user, token, history, userData, onNewUser, onSignOut} = this.props;
+  getUsers(token) {
+    axios.get('https://myflix-21.herokuapp.com/users', {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          users: response.data
+        });
+        console.log(response)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-    function updateInfo(token) {
-      const userInput = document.getElementById('username');
-      const passInput = document.getElementById('password');
-      const passVerInput = document.getElementById('passwordVer');
-      const emailInput = document.getElementById('email');
-      const dateInput = document.getElementById('DOB');
+  removeFavorite(movie) {
+    const token = localStorage.getItem("token");
+    const url =
+      "https://https://myflix-21.herokuapp.com/users" +
+      localStorage.getItem("user") +
+      "/movies/" +
+      movie._id;
+    axios
+      .delete(url, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        console.log(response);
+        this.componentDidMount();
+        // location.reload();
+        alert(movie.Title + " has been removed from your Favorites.");
+      });
+  }
 
-      if (userInput.value.length > 12) {
-        const userErr = document.getElementById('user');
-        return userErr.innerText = "Username can only be 12 characters";
-      }
+  handleDelete() {
+    const token = localStorage.getItem("token");
+    const user = localStorage.getItem("user");
+    axios.delete(`https://myflix-21.herokuapp.com/users/${user}`,
+      { headers: { Authorization: `Bearer ${token}` } }
+    )
+      .then(() => {
+        alert(user + " has been deleted.");
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
+        window.location.pathname = "/";
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-      const nameChoice = userInput.value || userData.Username;
-      let passChoice = null;
-      if (passInput.value == "") {
-        passChoice = "";
-      } else {
-        passChoice = passInput.value;
-      }
-      const emailChoice = emailInput.value || userData.Email;
-      const dateChoice = dateInput.value || userData.DOB;
-
-      if (passInput.value === passVerInput.value) {
-        axios.put(`https://myflix-21.herokuapp.com/users/${user}`, 
-        { 
-          Username: nameChoice, Password: passChoice, Email: emailChoice, DOB: dateChoice 
+  handleUpdate(e) {
+    let token = localStorage.getItem("token");
+    // console.log({ token });
+    let user = localStorage.getItem("user");
+    console.log(this.state);
+    let setisValid = this.formValidation();
+    if (setisValid) {
+      axios.put(`https://myflix-21.herokuapp.com/users/${user}`,
+        {
+          Username: this.state.Username,
+          Password: this.state.Password,
+          Email: this.state.Email,
+          Birthdate: this.state.Birthdate,
         },
-        { headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`}}
-        )
-        .then(response => {
-          console.log('Success with updating account information');
-          let userData2 = response.data;
-          onNewUser(userData2);
-          if (userInput.value != "") {
-            window.location = `/users/${userData2.Username}`;
-          }
-          if (passChoice != "") {
-            window.location = `/users/${userData2.Username}`;
-          }
+        { headers: { Authorization: `Bearer ${token}` } }
+      )
+        .then((response) => {
+          const data = response.data;
+          localStorage.setItem("user", data.Username);
+          console.log(data);
+          alert(user + " has been updated.");
+          console.log(response);
         })
         .catch(function (error) {
-          console.log(error);
+          console.log(error.response.data);
         });
-      } else {
-        const passErr = document.getElementById('pass');
-        const passErrVer = document.getElementById('passVer');
-        passErr.innerText = "The passwords must match";
-        passErrVer.innerText = "The passwords must match";
-      }
     }
+  }
 
-    //deleting an account 
-    function deleteAcc(token) {
-      axios.delete(`https://myflix-21.herokuapp.com/users/${user}`, 
-      { headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`}})
-      .then(response => {
-        console.log(response);
-        console.log(`${user} has been deleted`);
-      })
-      .catch(e => {
-        console.log('There is an error');
-        console.log(e);
-      })
+  formValidation() {
+    let UsernameError = {};
+    let EmailError = {};
+    let PasswordError = {};
+    let BirthdateError = {};
+    let isValid = true;
+    if (this.state.Username.trim().length < 5) {
+      UsernameError.usernameShort = "Must be alphanumeric and contains at least 5 characters";
+      isValid = false;
     }
-    
-
-    function Date() {
-      const formDate = userData.DOB;
-
-      return formDate.slice(0, 10);
+    if (this.state.Password.trim().length < 3) {
+      PasswordError.passwordMissing = "You must enter a current or new password.(minimum 4 characters) ";
+      isValid = false;
     }
-
-    if (userData.Username === 'testuser') {
-      return (
-        <>
-        <div className="centerProfile">
-          <h1 className="title my-4">Hello {`${userData.Username}`},</h1>
-          <h2 className="title-2 my-4">Current Information</h2>
-          <div className="align-text-left">
-            <div className=" my-2"><strong>Username:</strong> {`${userData.Username}`}</div>
-            <div className=" my-2"><strong>Email:</strong> {`${userData.Email}`}</div>
-            <div className=" my-2"><strong>Date of Birth:</strong> {`${Date()}`}</div>
-          </div>
-            <h2 className="title-2 my-4">Update Information</h2>
-            <div>The testuser account info cannot be updated!</div>
-          </div>
-        </>
-      );
+    if (!(this.state.Email && this.state.Email.includes(".") && this.state.Email.includes("@"))) {
+      EmailError.emailNotEmail = "A valid email address is required.";
+      isValid = false;
     }
+    if (this.state.birthdate === '') {
+      BirthdateError.birthdateEmpty = "Please enter your birthdate.";
+      isValid = false;
+    }
+    this.setState({
+      UsernameError: UsernameError,
+      PasswordError: PasswordError,
+      EmailError: EmailError,
+      BirthdateError: BirthdateError,
+    })
+    return isValid;
+  };
+
+  handleChange(e) {
+    let { name, value } = e.target;
+    // console.log(name, value);
+    this.setState({
+      [name]: value
+    })
+  }
+
+  render() {
+    const { user, movies } = this.props;
+    const { UsernameError, EmailError, PasswordError, BirthdateError } = this.state;
+    const FavoriteMovieList = movies.filter((movie) => {
+      return this.state.FavoriteMovies.includes(movie._id);
+    });
     return (
-      <>
-      <div className="centerProfile">
-        <h1 className="title my-4">Hello {`${userData.Username}`},</h1>
-        <h2 className="title-2 my-4">Current Information</h2>
-        <div className="align-text-left">
-          <div className=" my-2"><strong>Username:</strong> {`${userData.Username}`}</div>
-          <div className=" my-2"><strong>Email:</strong> {`${userData.Email}`}</div>
-          <div className=" my-2"><strong>Date of Birth:</strong> {`${Date()}`}</div>
-        </div>
-          <h2 className="title-2 my-4">Update Information</h2>
-          <form noValidate className="form">
+      <div className="userProfile" style={{ display: "flex" }}>
+        <Container>
+          <Row className="justify-content-md-center">
+            <Col md={12}>
+              <Jumbotron>
+                <div className="favoriteMovies" style={{ float: "center", textAlign: "center" }}>
+                  <Card.Text className="mt-200" as='h3'>Favorite Movies:</Card.Text>
+                  <Row className='mb-20'>
+                    {FavoriteMovieList.map((movie) => {
+                      return (
+                        <Col md={3} key={movie._id}>
+                          <div key={movie._id}>
+                            <Card className='mb-20'>
+                              <Card.Img variant="top" src={movie.ImagePath} />
+                              <Card.Body>
+                                <Link to={`/movies/${movie._id}`}>
+                                  <Card.Title as='h6'>{movie.Title}</Card.Title>
+                                </Link>
+                                <Button className='mb-30' onClick={() => this.removeFavorite(movie)}>Remove</Button>
+                              </Card.Body>
+                            </Card>
+                          </div>
+                        </Col>
+                      );
+                    })}
+                  </Row>
+                </div>
+              </Jumbotron>
 
-            <div className="input-wrap">
-              <label htmlFor="username">Username:</label>
-              <input type="text" id="username" placeholder="New Username"/>
-              <div id="user" className="error"></div>
-            </div>
+              <Jumbotron className="profile-view">
+                <Form className="justify-content-md-center">
+                  <h1 style={{ textAlign: "center" }}>Update Profile Details</h1>
+                  <Form.Group controlId="formUsername">
+                    <Form.Label>Username: </Form.Label>
+                    <FormControl size="sm"
+                      type="text"
+                      name="Username"
+                      value={this.state.Username}
+                      onChange={(e) => this.handleChange(e)}
+                      placeholder="Change username" />
+                    {Object.keys(UsernameError).map((key) => {
+                      return (
+                        <div key={key} style={{ color: "red" }}>
+                          {UsernameError[key]}
+                        </div>
+                      );
+                    })}
+                  </Form.Group>
+                  <Form.Group controlId="formPassword">
+                    <Form.Label>Password: </Form.Label>
+                    <FormControl size="sm"
+                      type="password"
+                      name="Password"
+                      value={this.state.Password}
+                      onChange={(e) => this.handleChange(e)}
+                      placeholder="Enter current password or Change password" />
+                    {Object.keys(PasswordError).map((key) => {
+                      return (
+                        <div key={key} style={{ color: "red" }}>
+                          {PasswordError[key]}
+                        </div>
+                      );
+                    })}
+                  </Form.Group>
+                  <Form.Group controlId="formEmail">
+                    <Form.Label>Email: </Form.Label>
+                    <FormControl
+                      size="sm"
+                      type="email"
+                      name="Email"
+                      value={this.state.Email}
+                      onChange={(e) => this.handleChange(e)}
+                      placeholder="Change Email" />
+                    {Object.keys(EmailError).map((key) => {
+                      return (
+                        <div key={key} style={{ color: "red" }}>
+                          {EmailError[key]}
+                        </div>
+                      );
+                    })}
+                  </Form.Group>
+                  <Form.Group controlId="formBirthdate">
+                    <Form.Label>Date of Birth: </Form.Label>
+                    <FormControl
+                      size="sm"
+                      type="date"
+                      name="Birthdate"
+                      value={this.state.Birthdate}
+                      onChange={(e) => this.handleChange(e)}
+                      placeholder="Change Birthdate" />
+                    {Object.keys(BirthdateError).map((key) => {
+                      return (
+                        <div key={key} style={{ color: "red" }}>
+                          {BirthdateError[key]}
+                        </div>
+                      );
+                    })}
+                  </Form.Group>
 
-            <div className="input-wrap">
-              <label htmlFor="password">Password:</label>
-              <input id="password" type="password" placeholder="New Password"/>
-              <div id="pass" className="error"></div>
-            </div>
-
-            <div className="input-wrap">
-              <label htmlFor="password Verification">Verify Password:</label>
-              <input id="passwordVer" type="password" placeholder="Verify Password Change"/>
-              <div id="passVer" className="error"></div>
-            </div>
-
-            <div className="input-wrap">
-              <label htmlFor="email">Email:</label>
-              <input id="email" type="email" placeholder="New Email"/>
-              <div id="email-err" className="error"></div>
-            </div>
-
-            <div className="input-wrap">
-              <label htmlFor="DOB">Date of Birth:</label>
-              <input id="DOB" type="Date"/>
-              <div id="Date" className="error"></div>
-            </div>
-            
-            <div className="middle">
-              <Button className="m-3 bttn" variant="info" type="button" onClick={() =>{updateInfo(token)}}>Update</Button>
-              <Link to={`/`}>
-                <Button className="m-3 bttn" variant="info" type="button">Go Back</Button>
-              </Link>
-              <Button className="m-3 bttn" variant="info" type="button" onClick={ () => { deleteAcc(token); onSignOut(null); history.push('/'); } }>Delete Account</Button>
-            </div>
-          </form>
-        </div>
-      </>
+                  <Link to={`/users/${this.state.Username}`}>
+                    <Button className="mb-2" variant="success"
+                      type="link"
+                      size="md"
+                      block
+                      onClick={(e) => this.handleUpdate(e)}
+                    >
+                      Save changes
+                    </Button>
+                  </Link>
+                </Form>
+              </Jumbotron>
+              <Jumbotron>
+                <h1 style={{ textAlign: "center" }}>Delete Account</h1>
+                <h3 style={{ textAlign: "center" }}>There is no undoing this action.</h3>
+                <br></br>
+                <Button className="mb-2" variant="danger" size="lg" block onClick={() => this.handleDelete()}> Delete Account
+                </Button>
+              </Jumbotron>
+            </Col>
+          </Row>
+        </Container>
+      </div >
     );
   }
 }
+ProfileView.propTypes = {
+  movies: PropTypes.array.isRequired
+};
+
+let mapStateToProps = state => {
+  return {
+    user: state.user,
+    movies: state.movies
+  }
+}
+
+//export default //connect(mapStateToProps, { setUser, updateUser })(ProfileView);
