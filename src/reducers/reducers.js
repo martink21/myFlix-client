45fe7e592f32dd5d4b9b4b24339f090576cee4a8
Martin Karastoyanov <martin.karastoyanov@gmail.com>
2021-08-06 14:06:47 +0200

    Created a new reducer userData

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import { SET_FILTER, SET_MOVIES, SET_USER} from '../actions/actions';
+import { SET_FILTER, SET_MOVIES, SET_USER, SET_USER_DATA } from '../actions/actions';
 
 function visibilityFilter(state = '', action) {
   switch (action.type) {
@@ -29,10 +29,21 @@ function user(state = '', action) {
     }
 }
 
+function userData(state = '', action) {
+  switch (action.type){
+      case SET_USER_DATA:
+          return action.value;
+      default:
+          return state;
+  }
+}
+
+
 const moviesApp = combineReducers({
   visibilityFilter,
   movies,
-  user
+  user,
+  userData
 });
 
 export default moviesApp;
